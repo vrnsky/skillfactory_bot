19ab87e38f501b43e9245247ebcf05611886c8da
Egor Voronianskii <voronyansky.e@notasoft.ru>
2020-12-17 12:11:32 +0800

    Initial commit #1

---
+++ src/main/java/ru/skillfactorydemo/tgbot/controllers/CurrencyController.java
@@ -0,0 +1,21 @@
+package ru.skillfactorydemo.tgbot.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skillfactorydemo.tgbot.dto.ValuteCursOnDate;
+import ru.skillfactorydemo.tgbot.service.CentralRussianBankService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CurrencyController {
+
+    private final CentralRussianBankService centralRussianBankService;
+
+    @GetMapping("/getCurrencies")
+    public List<ValuteCursOnDate> getValuteCursOnDate() throws Exception {
+        return centralRussianBankService.getCurrenciesFromCbr();
+    }
+}
